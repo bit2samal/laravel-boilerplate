882812aaf431dc8c21f0e1d12bd9c35143ee9b8d
Anil kumar <b2k8786@gmail.com>
2021-10-27 17:41:26 +0530

    test error

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/test_error', function () {
+    
+    return "Hi $call";
+    
 });
