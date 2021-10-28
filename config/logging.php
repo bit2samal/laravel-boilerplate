e1ad314e97326052e76775fa5a547b42111446e6
Anil kumar <b2k8786@gmail.com>
2021-10-28 18:44:14 +0530

    re-config

--- config/logging.php
+++ config/logging.php
@@ -17,7 +17,7 @@ return [
     |
     */
 
-    'default' => env('LOG_CHANNEL', 'rollbar'),
+    'default' => env('LOG_CHANNEL', 'stack'),
 
     /*
     |--------------------------------------------------------------------------
